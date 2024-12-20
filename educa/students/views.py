4e5b6bb051e9f692df106211d1e4f28ae157d03a
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-20 06:53:01 +0100

    student courses list and detail views

--- educa/students/views.py
+++ educa/students/views.py
@@ -8,6 +8,11 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
 from .forms import CourseEnrollForm
+# imports for displaying all courses enrolled by the students
+from django.views.generic.list import ListView
+from courses.models import Course
+# imports for showing details of each course
+from django.views.generic.detail import DetailView
 
 
 # Create your views here.
@@ -26,7 +31,7 @@ class StudentRegistrationView(CreateView):
         login(self.request, user)
         return result
 
-# view to enrol students in courses
+# view to enroll students in courses
 class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     course = None
     form_class = CourseEnrollForm
@@ -41,3 +46,35 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
             'student_course_detail', args=[self.course.id]
         )
 
+# view to display all enrolled courses
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+     def get_queryset(self):
+         qs = super().get_queryset()
+         return qs.filter(students__in=[self.request.user])
+
+
+# view to display details of a course
+class StudentCourseDetailView(LoginRequiredMixin, DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # get current module
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id']
+            )
+        else:
+            # get first module
+            context['module'] = course.modules.all()[0]
+        return context
