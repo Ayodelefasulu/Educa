2594deacb7f0d0b8e9656be5a5ee04596782e821
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-22 10:05:19 +0100

    api development. serializer and consuming data of course model

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -3,12 +3,16 @@ from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
 # import to add django aggregate function
 from django.db.models import Count
+# import to add pagination
+from courses.api.pagination import StandardPagination
+
 
 
 class SubjectListView(generics.ListAPIView):
     # queryset = Subject.objects.all()
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination # adding pagination
 
 class SubjectDetailView(generics.RetrieveAPIView):
     # queryset = Subject.objects.all()
