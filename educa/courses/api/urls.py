ee7060600ba6b75bcba11ce1f3454173740251e2
ayodelefasulu <fasuluayodele@gmail.com>
2025-01-05 05:52:56 +0100

    viewsets and routers both courses and subjects

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+# Import for router
+from rest_framework import routers
 
 app_name = "courses"
 
+# Router instance
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
+router.register('subjects', views.SubjectViewSet)
+
 urlpatterns = [
-    path("subjects/", views.SubjectListView.as_view(), name="subject_list"),
-    path("subjects/<pk>/", views.SubjectDetailView.as_view(), name="subject_detail"),
+    #path("subjects/", views.SubjectListView.as_view(), name="subject_list"),
+    #path("subjects/<pk>/", views.SubjectDetailView.as_view(), name="subject_detail"),
+    path("", include(router.urls)), # router implemented
 ]
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.models import Subject, Course
 
 # import to add django aggregate function
 from django.db.models import Count
@@ -8,15 +8,31 @@ from django.db.models import Count
 # import to add pagination
 from courses.api.pagination import StandardPagination
 
+# import for viewset
+from rest_framework import viewsets
 
+
+"""
 class SubjectListView(generics.ListAPIView):
     # queryset = Subject.objects.all()
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination  # adding pagination
+"""
 
+# implementing the subject viewset; commented out SubjectListView
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
 class SubjectDetailView(generics.RetrieveAPIView):
     # queryset = Subject.objects.all()
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
+
+# implementing course viewset
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
